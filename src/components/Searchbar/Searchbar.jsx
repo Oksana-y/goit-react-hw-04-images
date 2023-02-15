a8808ebde8c85614a45d920cab33b0ab53a0caeb
Oksana Yerep <114570807+Oksana-y@users.noreply.github.com>
2023-02-15 16:19:26 +0200

    Add changes

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import css from './searchbar.module.scss';
 
@@ -7,7 +7,6 @@ const Searchbar = ({ onSubmit }) => {
 
   const handleChange = e => {
     setQuery(e.target.value);
-    console.log(e.target.value);
   };
 
   const handleSubmit = e => {
